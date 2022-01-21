0ddd12e3e5c3fc0bfd77f1977bbf37b6afa72b4c
Rehaan-Kumar <73431008+Rehaan-Kumar@users.noreply.github.com>
2022-01-21 10:34:35 +0530

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,16 @@
+img = "";
+objects = [];
+model_status = "";
+
+function preload() {
+    img = loadImage("Bottles.jpg")
+}
+
+function setup() {
+    canvas = createCanvas(620 , 460)
+    canvas.center()
+}
+
+function draw() {
+    image(img,0,0,620,460)
+}
